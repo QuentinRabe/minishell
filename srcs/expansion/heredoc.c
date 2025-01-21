99413b76de2796b1c426247f0861b8197ab05f8a
QuentinRabe <lovegoodandyh@gmail.com>
2025-01-21 14:14:27 +0300

    (expansion): handle $"$PWD" expansion in hd

--- srcs/expansion/heredoc.c
+++ srcs/expansion/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: arabefam <arabefam@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 07:38:27 by arabefam          #+#    #+#             */
-/*   Updated: 2025/01/21 06:58:08 by arabefam         ###   ########.fr       */
+/*   Updated: 2025/01/21 14:13:56 by arabefam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static void	build_new_token_hd(char **input, t_var *list, int i[3])
 	i[1] = 0;
 	while ((*input)[i[0]])
 	{
-		if ((*input)[i[0]] == '$' && list)
+		if ((*input)[i[0]] == '$' && list && list->index == i[0])
 		{
 			i[2] = 0;
 			while (list->value[i[2]])
@@ -60,7 +60,9 @@ t_var **list, t_var_env *env)
 	new->index = *i;
 	new->next = NULL;
 	new->varname = get_varname_hd(token, *i + 1);
+	printf("varname->%s\n", new->varname);
 	new->value = get_env(env, new->varname);
+	printf("value->%s\n", new->value);
 	*i += ft_strlen(new->varname);
 	if (*list == NULL)
 		*list = new;
@@ -78,7 +80,9 @@ static void	create_var_list_hd(char *token, t_var_env *env, t_var **list)
 	i = 0;
 	while (token[i])
 	{
-		if (token[i] == '$')
+		if (token[i] == '$' && token[i + 1]
+			&& (ft_isalnum(token[i + 1])
+				|| token[i + 1] == '_'))
 			add_to_var_list_hd(token, &i, list, env);
 		if (token[i])
 			i++;
